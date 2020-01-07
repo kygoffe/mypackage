180762b41c63930399d193d54cf9c719266b61e8
kygoffe <a7912741@newcastle.ac.uk>
2020-01-07 12:21:55 +0000

    another commit

--- R/sum_square_function.R
+++ R/sum_square_function.R
@@ -1,3 +1,16 @@
+#' Calculate the sums of squares for a sample
+#'
+#' returns the sum of the squared deviations
+#'
+#' from mean, sums of squares(x) of a numeric vector
+#'
+#' @param v numeric vector
+#'
+#' @return number
+#' @export
+#'
+#' @examples
+#' sum_squares_function(rnorm(50))
 sum_squares_function <- function(v){
   sum((v - mean(v))^2)
 }
